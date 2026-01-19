6568f9649597a3f2bcd10852fbfd90a64b88be1e
jkenny264 <johnkenny264@gmail.com>
2026-01-18 23:32:05 -0500

    add q1, q2, q5

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -4,3 +4,31 @@
 
 # Question 2: Defining an array (list)
 my_array = [1, 4, 7, 9]
+
+
+
+
+
+
+# Question 3:
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed Version of: e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result:", e)
+
+
+# Question 4:
+temperature = 32.6
+print("The temperature today is: {:.3f} degree Celcius".format(temperature))
+
+
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtered by age: ", userAge)
